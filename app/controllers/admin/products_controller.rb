2435dd1e9ae4b091df01bd043d968453f2ad8570
Reverson <reverson.santos@irancho.com.br>
2025-06-10 12:07:18 -0300

    products working well

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -25,7 +25,7 @@ class Admin::ProductsController < AdminController
 
     respond_to do |format|
       if @admin_product.save
-        format.html { redirect_to @admin_product, notice: "Product was successfully created." }
+        format.html { redirect_to admin_product_url(@admin_product), notice: "Product was successfully created." }
         format.json { render :show, status: :created, location: @admin_product }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class Admin::ProductsController < AdminController
   def update
     respond_to do |format|
       if @admin_product.update(admin_product_params)
-        format.html { redirect_to @admin_product, notice: "Product was successfully updated." }
+        format.html { redirect_to admin_product_url(@admin_product), notice: "Product was successfully updated." }
         format.json { render :show, status: :ok, location: @admin_product }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class Admin::ProductsController < AdminController
     @admin_product.destroy!
 
     respond_to do |format|
-      format.html { redirect_to admin_products_path, status: :see_other, notice: "Product was successfully destroyed." }
+      format.html { redirect_to admin_products_url, notice: "Product was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -65,6 +65,6 @@ class Admin::ProductsController < AdminController
 
     # Only allow a list of trusted parameters through.
     def admin_product_params
-      params.require(:product).permit(:name, :description, :price, :category_id, :active)
+      params.require(:product).permit(:name, :description, :price, :category_id, :active, images: [])
     end
 end
